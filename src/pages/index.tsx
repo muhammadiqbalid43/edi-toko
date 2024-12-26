c054a4318f91061d322b117b5f4d4c54f0038bca
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2024-12-26 16:04:06 +0700

    fix: conflik

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,14 @@
-import Hero from '@/components/Hero';
+import Hero from "@/components/Hero";
+import Category from "@/components/category";
 import Footer from "@/components/footer";
-
-import localFont from "next/font/local";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export default function Home() {
   return (
     <div>
       <h1>Hello world</h1>
-
       <Hero />
-
       <Category />
-main
+      main
       <Footer />
     </div>
   );
